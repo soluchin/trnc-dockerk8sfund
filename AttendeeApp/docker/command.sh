7e4195027cfac0b7895f415dea16403441313e9f
soluchin <rafisolichin@gmail.com>
2024-10-16 21:28:02 +0700

    update attendee app

--- AttendeeApp/docker/command.sh
+++ AttendeeApp/docker/command.sh
@@ -1,3 +1,5 @@
 # create mysql container
 docker container create --name mysqldb --publish 9901:3306 -e MYSQL_ROOT_PASSWORD=r00t@Password -e MYSQL_DATABASE=mydb -e MYSQL_USER=node -e MYSQL_PASSWORD=node123 mysql
 
+# build docker image
+docker build -t attendeeapp .
